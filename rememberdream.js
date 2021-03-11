4f6c52cbe92265d46359c5556adead7c8e759d3c
sdc99310 <sdc99310@gmail.com>
2021-03-10 23:30:34 -0500

    Create rememberdream.js

---
+++ rememberdream.js
@@ -0,0 +1,18 @@
+document.addEventListener('mousemove',function(e){
+    const scratch = document.createElement('span');
+    const body = document.querySelector('body');
+
+    scratch.style.top = e.offsetY + 'px';
+    scratch.style.left = e.offsetX + 'px';
+    
+    body.appendChild(scratch);
+
+});
+
+
+
+
+/* work cited */
+// https://www.w3schools.com/jsref/met_node_appendchild.asp
+// https://www.w3schools.com/tags/tag_span.asp
+// https://youtu.be/qzcMiNKPSIk
